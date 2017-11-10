41727da6a242055cfe404905c9f38a4f1eb586da
Arsenal591 <arsenal591@126.com>
2017-11-10 20:55:20 +0800

    添加操作数类型

--- instruction_table.asm
+++ instruction_table.asm
@@ -1,6 +1,7 @@
 .386
 .MODEL flat, stdcall
 
+include operand.inc
 include instruction_table.inc
 
 public table_mapping
@@ -9,11 +10,20 @@ public table_mapping
 ; -------- tables begin here --------
 ; (the following code is just an example)
 
-AND_table_elems TableElem \
-	<0,0>,\
-	<7,1>,\
-	<5,2>
-AND_table Table <3, OFFSET AND_Table_elems>
+ADD_table_elems TableElem \
+	<80h, reg_or_mem_type, 08h, imm_type, 08h, 0, 0 >, \
+	<81h, reg_or_mem_type, 10h, imm_type, 10h, 0, 0 >, \
+	<81h, reg_or_mem_type, 20h, imm_type, 20h, 0, 0 >, \
+	<83h, reg_or_mem_type, 10h, imm_type, 08h, 0, 0 >, \
+	<83h, reg_or_mem_type, 20h, imm_type, 08h, 0, 0 >, \
+	<00h, reg_or_mem_type, 08h, reg_type, 08h, 0, 0 >, \
+	<01h, reg_or_mem_type, 10h, reg_type, 10h, 0, 0 >, \
+	<01h, reg_or_mem_type, 20h, reg_type, 20h, 0, 0 >, \
+	<02h, reg_type, 08h, reg_or_mem_type, 08h, 0, 0 >, \
+	<03h, reg_type, 10h, reg_or_mem_type, 10h, 0, 0 >, \
+	<03h, reg_type, 20h, reg_or_mem_type, 20h, 0, 0 >
+
+ADD_table Table <LENGTHOF ADD_table_elems, OFFSET AND_Table_elems>
 
 ; ----------- Above this line is WJL's work(tables A - M)
 ; below this line is JYH's work(tables N - Z) -----------
