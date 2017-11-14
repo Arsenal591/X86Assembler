f13dd4edc619e64e77f42fc0cd81995a0ee2931c
MrJia1997 <jia379853818@hotmail.com>
2017-11-14 20:49:02 +0800

    Add .code judge in tokenize data segment

--- main.asm
+++ main.asm
@@ -7,17 +7,17 @@ include Irvine32.inc
 includelib Irvine32.lib
 
 .data
-mme BYTE "MOV", 0
-op1_reg RegOperand <EDX_num>
-op1 Operand <reg_type, 20h, OFFSET op1_reg>
-op2_reg IMMOperand <01234h>
-op2 Operand <imm_type, 20h, OFFSET op2_reg>
+mme BYTE "ADD", 0
+op1_reg GlobalOperand <028h>
+op1 Operand <global_type, 20h, OFFSET op1_reg>
+op2_reg IMMOperand <08h>
+op2 Operand <imm_type, 08h, OFFSET op2_reg>
 result BYTE 50 DUP(0)
 test_string BYTE " 25 * ecx + ebx ", 0
 local_result LocalOperand <>
 .code
 main proc
-	; INVOKE translate_asm_to_machine_code, ADDR mme, ADDR op1, ADDR op2, ADDR result
+	INVOKE translate_asm_to_machine_code, ADDR mme, ADDR op1, ADDR op2, ADDR result
 	; INVOKE parse_local_operand, ADDR test_string, ADDR local_result
 	
 	INVOKE ExitProcess,0
