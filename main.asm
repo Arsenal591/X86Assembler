d8d5f0711095c0bb5c5459b03c32eede97559470
Arsenal591 <arsenal591@126.com>
2017-11-15 19:57:40 +0800

    添加调试代码

--- main.asm
+++ main.asm
@@ -37,10 +37,17 @@ convert_symbol_to_operand PROTO,
 	pOperand: DWORD,
 	current_address: DWORD
 
+tokenize_code_segment PROTO,
+	pCode: PTR BYTE,
+	max_length: DWORD
+
 .data
+test11 DWORD ?
 test_local_operand LocalOperand < 0,0,0,0 >
 test_operand Operand <0,0, offset test_local_operand>
-test_code byte "MOV EAX EBX", 13, 10, "ADD abc 8", 0
+test_code byte "A PROC ", 13, 10, "ADD abc 8", 13, 10, "MOV EAX EBX", 13, 10, "A ENDP", 13, 10, "B PROC", 13, 10, "JMP A", 13, 10, "B ENDP", 0
+test_code1 byte "A PROC", 13, 10, "MOV [EBP] 16", 13, 10, "MOV [EDX + 4*EAX+4] 10 ", 0
+test_code2 byte "RET", 0
 initial_address DWORD 0
 char_processed DWORD 0
 final_address DWORD 0
@@ -54,18 +61,24 @@ test_symbol6 BYTE "qrs", 0
 
 test_str BYTE "qddrs", 0
 
-test_data_seg BYTE "var12345 dword 123", 0ah, "var2 byte ", 022h, "test", 022h, 0ah, ".code"
 .code
 main proc
 
 	invoke push_list, offset data_symbol_list, offset test_symbol1, 40, 0
 	invoke push_list, offset data_symbol_list, offset test_symbol2, 50, 0
 
-	invoke Str_length, offset test_code
-	invoke tokenize_instruction, offset test_code, eax,\
-	       initial_address, offset char_processed, \
-		   offset final_address
-	
+	invoke Str_length, offset test_code2
+	invoke tokenize_instruction, offset test_code2, eax, \ 
+			initial_address, offset char_processed, offset final_address
+	;invoke tokenize_code_segment, offset test_code, eax
+	;invoke tokenize_instruction, offset test_code, eax,\
+	;       initial_address, offset char_processed, \
+	;	   offset final_address
+	;mov eax, char_processed
+	;invoke WriteInt
+	;mov eax, final_address
+	;invoke WriteInt
+	INVOKE ExitProcess,0
 	mov ecx, 0
 	mov ebx, code_symbol_list.address
 	.while ecx < code_symbol_list.len
@@ -98,6 +111,9 @@ main proc
 	!
 	ret
 
+	; INVOKE translate_asm_to_machine_code, ADDR mme, ADDR op1, ADDR op2, ADDR result
+	; INVOKE parse_local_operand, ADDR test_string, ADDR local_result
+	
 	INVOKE ExitProcess,0
 main endp
 end main
