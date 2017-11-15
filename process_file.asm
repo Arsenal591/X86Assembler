6307b69c428838fc9e0c6586ec551b853a7f4318
MrJia1997 <jia379853818@hotmail.com>
2017-11-15 16:14:33 +0800

    Add process file

--- process_file.asm
+++ process_file.asm
@@ -10,6 +10,26 @@ BUFFER_SIZE = 5000
 buffer BYTE BUFFER_SIZE DUP(0)
 .code
 ;--------------------------------------------------
+judge_data_segment PROC USES ebx ecx edi,
+	str_addr: PTR BYTE
+; Return : al = 1 is code, else is not
+;--------------------------------------------------
+	mov edi, str_addr
+	mov bh, [edi + 1]
+	mov bl, [edi + 2]
+	mov ch, [edi + 3]
+	mov dh, [edi + 4]
+	
+	.IF (bh == 100) && (bl == 97) && (ch == 116) && (cl == 97)
+		mov al, 1
+	.ELSE
+		mov al, 0
+	.ENDIF
+
+	ret
+judge_data_segment ENDP
+
+;--------------------------------------------------
 process_file PROC USES eax ecx edx esi,
 	file_path: PTR BYTE ; the string of file path
 ;
@@ -34,7 +54,21 @@ process_file PROC USES eax ecx edx esi,
 success_read:
 	mov esi, OFFSET buffer
 	; find .data and add esi
+	mov ecx, bytesRead
+loop_read:
+	mov bl, [esi]
+	.IF bl == 46 ; dot
+		INVOKE judge_data_segment, esi
+		.IF al == 1
+			add esi, 5
+			jmp find_data
+		.ENDIF
+	.ENDIF
+	inc esi
+	loop loop_read
+	ret
 
+find_data:
 	; tokenize data segment
 	INVOKE tokenize_data_segment, esi, bytesRead
 	add esi, eax
